e5a8bd34bd566f27963cf553612e13d4665ebd5c
Rune Flobakk <flobakk@gmail.com>
2010-04-15 01:29:27 +0200

    forgot to commit modified files. Still getting used to git...

--- src/main/java/no/bekk/javafaggruppe/Lamp.java
+++ src/main/java/no/bekk/javafaggruppe/Lamp.java
@@ -1,22 +1,83 @@
 package no.bekk.javafaggruppe;
 
-import java.util.HashSet;
-import java.util.Set;
+import static no.bekk.javafaggruppe.LightState.BROKEN;
+import static no.bekk.javafaggruppe.LightState.OFF;
+import static no.bekk.javafaggruppe.LightState.ON;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Lamp {
-    public final Set<Light> lights = new HashSet<Light>();
+
+    private final List<Light> lights = new ArrayList<Light>();
+
     public Lamp(Light ... lights) {
         for (Light light : lights) {
             this.lights.add(light);
         }
     }
 
+    public void turnOn() {
+        for (Light light : lights) {
+            if (!light.isCurrently(BROKEN)) {
+                light.setState(ON);
+            }
+        }
+    }
+
+    public void turnOff() {
+        for (Light light : lights) {
+            if (!light.isCurrently(BROKEN)) {
+                light.setState(OFF);
+            }
+        }
+    }
+
+
     public int getLightAmount() {
         return lights.size();
     }
 
-    public Set<Light> getLights() {
+    public List<Light> getLights() {
         return lights;
     }
+
+    public List<Light> getLightsWhichAre(LightState state) {
+        List<Light> lights = new ArrayList<Light>();
+        for (Light light : this.lights) {
+            if (light.isCurrently(state)) {
+                lights.add(light);
+            }
+        }
+        return lights;
+    }
+
+    public int getAmountOfLightsWhichAre(LightState state) {
+        int amount = 0;
+        for (Light light : lights) {
+            if (light.isCurrently(state)) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+
+    public int getPendingReplacementCosts() {
+        int totalPrice = 0;
+        for (Light light : getLightsWhichAre(BROKEN)) {
+            totalPrice += light.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void replaceBrokenLightWith(Light newLight) {
+        for (Light light : lights) {
+            if (light.isCurrently(BROKEN)) {
+                lights.remove(light);
+                lights.add(newLight);
+            }
+        }
+    }
+
 }
--- src/main/java/no/bekk/javafaggruppe/Light.java
+++ src/main/java/no/bekk/javafaggruppe/Light.java
@@ -3,16 +3,26 @@ package no.bekk.javafaggruppe;
 import static no.bekk.javafaggruppe.LightState.OFF;
 
 public class Light {
-    private LightState state = OFF;
 
-    public Light setState(LightState state) {
+    private final int price;
+
+    private LightState state;
+
+    public Light(int price) {
+        this.price = price;
+        this.state = OFF;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setState(LightState state) {
         this.state = state;
-        return this;
     }
 
-    public LightState getState() {
-        return state;
+    public boolean isCurrently(LightState state) {
+        return this.state == state;
     }
-
 
 }
